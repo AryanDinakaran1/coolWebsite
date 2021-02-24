1ab1c6718afe2fa9c0bbff7503059a73b528cddc
Aryan Dinakaran <aryandinakaran@gmail.com>
2021-02-24 15:40:23 +0000

    Added some more brains

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-document.addEventListener("DOMContentLoaded",function(){
-     document.querySelector("form").onsubmit = function()
+//Hello World
+let count = 0;
+function counter()
+{
+     count++;
+     document.querySelector("#count").innerHTML = count;
+
+     if(count%5===0)
      {
-          let fname = document.querySelector("#fname").value;
-          let lname = document.querySelector("#lname").value;
+          alert(`${count} is a multiple of 5`);
+     }
+}
+document.addEventListener("DOMContentLoaded",function()
+{
+     //Button
+     document.querySelectorAll("button").forEach(button => 
+     {
+          button.onclick = function() 
+          {
+               document.querySelector("h2").style.color = button.dataset.color;
+          };   
+     });
 
-          alert(`Hello, ${fname} ${lname}!`);
-
-     };
+     //Counter
+     document.querySelector("#count").onclick = counter;
 });
